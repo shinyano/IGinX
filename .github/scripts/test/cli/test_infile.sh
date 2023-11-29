8c707f0be4fa0ef0b6a82faa80dcc994bd6ab8c1
shinyano <12236590+shinyano@users.noreply.github.com>
2023-11-29 09:33:42 +0800

    fix test

--- .github/scripts/test/cli/test_infile.sh
+++ .github/scripts/test/cli/test_infile.sh
@@ -2,6 +2,16 @@
 
 set -e
 
-bash -c "chmod +x client/target/iginx-client-0.6.0-SNAPSHOT/sbin/start_cli.sh"
+WINDOWS_COMMAND="cmd.exe /c 'client\\target\\iginx-client-0.6.0-SNAPSHOT\\sbin\\start_cli.bat -e {}'"
+LINUX_COMMAND="bash client/target/iginx-client-0.6.0-SNAPSHOT/sbin/start_cli.sh -e '{}'"
 
-bash -c "echo 'LOAD DATA FROM INFILE "'"test/src/test/resources/fileReadAndWrite/csv/test.csv"'" AS CSV INTO t(key, a, b, c, d);' | xargs -0 -t -i bash client/target/iginx-client-0.6.0-SNAPSHOT/sbin/start_cli.sh -e '{}'"
+if [ -n "$MSYSTEM" ]; then
+    echo "Testing on Windows..."
+    SCRIPT_COMMAND=WINDOWS_COMMAND
+else
+    echo "Testing on Unix..."
+    SCRIPT_COMMAND=LINUX_COMMAND
+    bash -c "chmod +x client/target/iginx-client-0.6.0-SNAPSHOT/sbin/start_cli.sh"
+fi
+
+bash -c "echo 'LOAD DATA FROM INFILE "'"test/src/test/resources/fileReadAndWrite/csv/test.csv"'" AS CSV INTO t(key, a, b, c, d);' | xargs -0 -t -i ${SCRIPT_COMMAND}"
