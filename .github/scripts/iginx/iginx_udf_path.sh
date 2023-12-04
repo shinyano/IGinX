d88ef89b3ea2a6469c6c8f0a0354af2e542a9ffc
shinyano <12236590+shinyano@users.noreply.github.com>
2023-12-04 16:44:02 +0800

    test

--- .github/scripts/iginx/iginx_udf_path.sh
+++ .github/scripts/iginx/iginx_udf_path.sh
@@ -24,7 +24,9 @@ if [ -n "$MSYSTEM" ]; then
 #    sh -c "start /min iginx-core-0.6.0-SNAPSHOT/sbin/start_iginx.bat > ../../iginx-udf.log 2>&1"
 #    cmd.exe /c "iginx-core-0.6.0-SNAPSHOT/sbin/start_iginx.bat > ../../iginx.log 2>&1 &"
 #    sh -c "./iginx-core-0.6.0-SNAPSHOT/sbin/start_iginx.bat > ../../iginx-udf.log 2>&1"
-    start /B cmd.exe /C "CALL iginx-core-0.6.0-SNAPSHOT/sbin/start_iginx.bat > ../../iginx-udf.log 2>&1"
+#    batPath="$(find core/target -name 'start_iginx.bat' | grep 'iginx-core-.*\/sbin' | head -n 1)"
+    powershell -Command "Start-Process -FilePath 'iginx-core-0.6.0-SNAPSHOT/sbin/start_iginx.bat' -NoNewWindow -RedirectStandardOutput 'iginx-udf.log' -RedirectStandardError 'iginx-udf-error.log'"
+#    start /B cmd.exe /C "CALL iginx-core-0.6.0-SNAPSHOT/sbin/start_iginx.bat > ../../iginx-udf.log 2>&1"
 
 
     cat "../../iginx-udf.log"
