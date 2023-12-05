c9d92cebed3b7b38137e50a42e0968444db4865e
shinyano <12236590+shinyano@users.noreply.github.com>
2023-12-05 17:19:34 +0800

    test

--- .github/scripts/test/cli/test_infile.sh
+++ .github/scripts/test/cli/test_infile.sh
@@ -6,7 +6,7 @@ COMMAND='LOAD DATA FROM INFILE "'"test/src/test/resources/fileReadAndWrite/csv/t
 
 if [ -n "$MSYSTEM" ]; then
 
-	bash -c "client/target/iginx-client-0.6.0-SNAPSHOT/sbin/start_cli.bat -e $COMMAND"
+	bash -c "client/target/iginx-client-0.6.0-SNAPSHOT/sbin/start_cli.bat -e '$COMMAND'"
 else
 
 	SCRIPT_COMMAND="bash client/target/iginx-client-0.6.0-SNAPSHOT/sbin/start_cli.sh -e '{}'"
