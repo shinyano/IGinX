84995de5a54bfc436f8c11cb269892be08e4891f
shinyano <12236590+shinyano@users.noreply.github.com>
2023-12-05 19:33:18 +0800

    test

--- .github/scripts/metadata/zk_windows.sh
+++ .github/scripts/metadata/zk_windows.sh
@@ -11,3 +11,7 @@ sh -c "mv apache-zookeeper-3.7.2-bin zookeeper"
 sh -c "cp ./.github/actions/zookeeperRunner/zooWin.cfg zookeeper/conf/zoo.cfg"
 
 powershell -Command "Start-Process -FilePath 'zookeeper/bin/zkServer.cmd' -NoNewWindow -RedirectStandardOutput 'zookeeper/zookeeper.log' -RedirectStandardError 'zookeeper/zookeeper-error.log'"
+
+sh -c "sleep 10"
+
+sh -c "netstat -ano | findstr 2181"
