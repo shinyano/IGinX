d6de507039c03d45e617affc578cdf3887ac26c0
shinyano <12236590+shinyano@users.noreply.github.com>
2023-12-07 16:06:16 +0800

    test

--- .github/scripts/dataSources/mix_iotdb12_influxdb_windows.sh
+++ .github/scripts/dataSources/mix_iotdb12_influxdb_windows.sh
@@ -10,6 +10,6 @@ sh -c ".github/scripts/dataSources/iotdb12_windows.sh 6667"
 
 sh -c ".github/scripts/dataSources/influxdb_windows.sh"
 
-set -i "s/storageEngineList/#storageEngineList/g" conf/config.properties
+sed -i "s/storageEngineList/#storageEngineList/g" conf/config.properties
 
 echo "storageEngineList=127.0.0.1#6667#iotdb12#username=root#password=root#sessionPoolSize=50#has_data=false#is_read_only=false,127.0.0.1#8086#influxdb#url=http://localhost:8086/#token=testToken#organization=testOrg#has_data=false" >> conf/config.properties
---
+++ .github/scripts/metadata/etcd_windows.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+powershell -command "Invoke-WebRequest -Uri https://go.dev/dl/go1.21.5.windows-amd64.zip -OutFile go1.21.5.windows-amd64.zip -Progress Continue"
+
+powershell -command "Expand-Archive ./go1.21.5.windows-amd64.zip -DestinationPath './'"
+
+sh -c "ls ./"
+
+sh -c "ls go"
+
+export GOROOT=$PWD
+
+export GOPATH=$GOROOT/go
+
+export GOBIN=$GOPATH/bin
+
+export PATH=$PATH:$GOBIN
+
+echo $GOBIN
+
+powershell -command "Invoke-WebRequest -Uri https://github.com/etcd-io/etcd/releases/download/v3.4.28/etcd-v3.4.28-windows-amd64.zip -OutFile etcd-v3.4.28-windows-amd64.zip"
+
+powershell -command "Expand-Archive ./etcd-v3.4.28-windows-amd64.zip -DestinationPath './'"
+
+sh -c "mv etcd-v3.4.28-windows-amd64/etcd* $GOBIN"
+
+powershell -command "Start-Process etcd -NoNewWindow -RedirectStandardOutput 'etcd-run.log' -RedirectStandardError 'etcd-error.log'"
