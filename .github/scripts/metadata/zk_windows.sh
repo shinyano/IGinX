bc338256919c84c47e9c9c9662e2215aad70ddfe
shinyano <12236590+shinyano@users.noreply.github.com>
2023-12-09 14:40:10 +0800

    test

--- .github/scripts/metadata/zk_windows.sh
+++ .github/scripts/metadata/zk_windows.sh
@@ -12,19 +12,19 @@ sh -c "cp ./.github/actions/zookeeperRunner/zooWin.cfg zookeeper/conf/zoo.cfg"
 
 cd zookeeper
 
-sh -c "mkdir logs"
-
-sh -c "mkdir data"
-
-dataDir=$(cygpath -w "${PWD}/data" | sed 's/\\/\\\\/g')
-
-logDir=$(cygpath -w "${PWD}/logs" | sed 's/\\/\\\\/g')
-
-echo $logDir
-
-sed -i "s#^dataDir=.*#dataDir=$dataDir#" conf/zoo.cfg
-
-sed -i "s#^dataLogDir=.*#dataLogDir=$logDir#" conf/zoo.cfg
+#sh -c "mkdir logs"
+#
+#sh -c "mkdir data"
+#
+#dataDir=$(cygpath -w "${PWD}/data" | sed 's/\\/\\\\/g')
+#
+#logDir=$(cygpath -w "${PWD}/logs" | sed 's/\\/\\\\/g')
+#
+#echo $logDir
+#
+#sed -i "s#^dataDir=.*#dataDir=$dataDir#" conf/zoo.cfg
+#
+#sed -i "s#^dataLogDir=.*#dataLogDir=$logDir#" conf/zoo.cfg
 
 powershell -Command "Start-Process -FilePath 'bin/zkServer.cmd' -NoNewWindow -RedirectStandardOutput 'logs/zookeeper.log' -RedirectStandardError 'logs/zookeeper-error.log'"
 
