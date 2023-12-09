1d9cde7fc2207335562203415f63c16cf845083b
shinyano <12236590+shinyano@users.noreply.github.com>
2023-12-09 13:51:01 +0800

    test

--- .github/scripts/metadata/zk_windows.sh
+++ .github/scripts/metadata/zk_windows.sh
@@ -10,17 +10,31 @@ sh -c "mv apache-zookeeper-3.7.2-bin zookeeper"
 
 sh -c "cp ./.github/actions/zookeeperRunner/zooWin.cfg zookeeper/conf/zoo.cfg"
 
-sh -c "mkdir zookeeper/logs"
+cd zookeeper
 
-logDir=$(realpath "zookeeper/logs")
+sh -c "mkdir logs"
+
+sh -c "mkdir data"
+
+dataDir=$(realpath "data")
+
+logDir=$(realpath "logs")
 
 echo $logDir
+
+sed -i "s/^dataDir=.*/dataDir=$dataDir/" "conf/zoo.cfg"
+
+sed -i "s/^dataLogDir=.*/dataLogDir=$logDir/" "conf/zoo.cfg"
 
 powershell -Command "Start-Process -FilePath 'zookeeper/bin/zkServer.cmd' -NoNewWindow -RedirectStandardOutput '$logDir/zookeeper.log' -RedirectStandardError '$logDir/zookeeper-error.log'"
 
 sleep 3
 
-echo "ls zookeeper/data"
-sh -c "ls zookeeper/data"
-echo "ls zookeeper/logs"
-sh -c "ls zookeeper/logs"
+echo "ls data"
+sh -c "ls data"
+echo "ls logs"
+sh -c "ls logs"
+
+sleep 3
+
+sh -c "cat $logDir/zookeeper.log"
