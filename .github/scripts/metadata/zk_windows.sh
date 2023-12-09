b95da3ed576ff20488546decbce3edcbebe23026
shinyano <12236590+shinyano@users.noreply.github.com>
2023-12-09 14:34:57 +0800

    test

--- .github/scripts/metadata/zk_windows.sh
+++ .github/scripts/metadata/zk_windows.sh
@@ -16,9 +16,9 @@ sh -c "mkdir logs"
 
 sh -c "mkdir data"
 
-dataDir=$(cygpath -w "${PWD}/data")
+dataDir=$(cygpath -w "${PWD}/data" | sed 's/\\/\\\\/g')
 
-logDir=$(cygpath -w "${PWD}/logs")
+logDir=$(cygpath -w "${PWD}/logs" | sed 's/\\/\\\\/g')
 
 echo $logDir
 
@@ -26,7 +26,7 @@ sed -i "s#^dataDir=.*#dataDir=$dataDir#" conf/zoo.cfg
 
 sed -i "s#^dataLogDir=.*#dataLogDir=$logDir#" conf/zoo.cfg
 
-powershell -Command "Start-Process -FilePath 'bin/zkServer.cmd' -NoNewWindow -RedirectStandardOutput '$logDir/zookeeper.log' -RedirectStandardError '$logDir/zookeeper-error.log'"
+powershell -Command "Start-Process -FilePath 'bin/zkServer.cmd' -NoNewWindow -RedirectStandardOutput 'logs/zookeeper.log' -RedirectStandardError 'logs/zookeeper-error.log'"
 
 sleep 3
 
