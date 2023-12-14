2472a3a693cc63594a70834f51640f010016920b
shinyano <12236590+shinyano@users.noreply.github.com>
2023-12-14 20:32:02 +0800

    give iotdb 2g 2g heap

--- .github/scripts/dataSources/iotdb12_windows.sh
+++ .github/scripts/dataSources/iotdb12_windows.sh
@@ -14,19 +14,19 @@ sh -c "echo ========================="
 
 sh -c "ls apache-iotdb-0.12.6-server-bin"
 
+sh -c "sed -i 's/# wal_buffer_size=16777216/wal_buffer_size=167772160/g' apache-iotdb-0.12.6-server-bin/conf/iotdb-engine.properties"
+
+sh -c "sed -i 's/^@REM set MAX_HEAP_SIZE=.*$/set MAX_HEAP_SIZE=2G/g' apache-iotdb-0.12.6-server-bin/conf/iotdb-env.bat"
+
+sh -c "sed -i 's/^@REM set HEAP_NEWSIZE=.*$/set HEAP_NEWSIZE=2G/g' apache-iotdb-0.12.6-server-bin/conf/iotdb-env.bat"
+
+sh -c "sed -i 's/^# compaction_strategy=.*$/compaction_strategy=NO_COMPACTION/g' apache-iotdb-0.12.6-server-bin/conf/iotdb-engine.properties"
+
 for port in "$@"
 do
   sh -c "cp -r apache-iotdb-0.12.6-server-bin/ apache-iotdb-0.12.6-server-bin-$port"
 
-  sh -c "sed -i 's/# wal_buffer_size=16777216/wal_buffer_size=167772160/g' apache-iotdb-0.12.6-server-bin-$port/conf/iotdb-engine.properties"
-
   sh -c "sed -i 's/6667/$port/g' apache-iotdb-0.12.6-server-bin-$port/conf/iotdb-engine.properties"
-
-  sh -c "sed -i 's/^@REM set MAX_HEAP_SIZE=.*$/set MAX_HEAP_SIZE=1G/g' apache-iotdb-0.12.6-server-bin-$port/conf/iotdb-env.bat"
-
-  sh -c "sed -i 's/^@REM set HEAP_NEWSIZE=.*$/set HEAP_NEWSIZE=1G/g' apache-iotdb-0.12.6-server-bin-$port/conf/iotdb-env.bat"
-
-  sh -c "sed -i 's/^# compaction_strategy=.*$/compaction_strategy=NO_COMPACTION/g' apache-iotdb-0.12.6-server-bin-$port/conf/iotdb-engine.properties"
 
   sh -c "grep '^set MAX_HEAP_SIZE=' apache-iotdb-0.12.6-server-bin-$port/conf/iotdb-env.bat"
 
