6b252cc686e1b680e9a694913c220d091139aebe
shinyano <12236590+shinyano@users.noreply.github.com>
2024-03-05 17:01:29 +0800

    wip: test arrow c data interface

--- udf_funcs/python_scripts/iginx_udf/arrow/controller.py
+++ udf_funcs/python_scripts/iginx_udf/arrow/controller.py
@@ -5,3 +5,10 @@ import tempfile
 class ArrowController:
     def __init__(self):
         pass
+
+    def receive(self):
+        import pyarrow as pa
+        from pyarrow.cffi import ffi
+        # array = pa.Array._import_from_c()
+        c_stream = ffi.new("struct ArrowArrayStream*")
+
